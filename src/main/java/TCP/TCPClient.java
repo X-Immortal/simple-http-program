63dbcc652128249117cde0c455a60c3757a732cb
Immortal_X <2063345498@qq.com>
2025-12-04 22:04:25 +0800

    updated timeout.

--- src/main/java/TCP/TCPClient.java
+++ src/main/java/TCP/TCPClient.java
@@ -31,7 +31,7 @@ public class TCPClient {
         if (isReady()) return;
         try {
             clientSocket = new Socket(host, port);
-            clientSocket.setSoTimeout(1000);
+            clientSocket.setSoTimeout(100);
         } catch (IOException e) {
             System.out.println("Failed to connect to " + host + ": " + port);
         }
